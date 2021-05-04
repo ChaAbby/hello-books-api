6c45f69e644b73010fc84c69c975b84e56d185b7
Abigail Charime <abigail.charime@gmail.com>
2021-05-03 20:13:02 -0700

    add PUT functionality to code

--- app/routes.py
+++ app/routes.py
@@ -1,26 +1,10 @@
 from app import db
-from flask import jsonify
-from flask import Blueprint
-from flask import request
-from .models.book import Book
+from flask import jsonify, Blueprint, Response, request
+from app.models.book import Book
 
 
 books_bp = Blueprint("books", __name__, url_prefix="/books")
 
-@books_bp.route("/<book_id>", methods=["GET"])
-def handle_book(book_id):
-    book = Book.query.get(book_id)
-
-    if book:
-        return ({
-            "id": book.id,
-            "title": book.title,
-            "description": book.description
-        }, 200)
-    return{
-        "message": f"Book {book_id} not found",
-        "success": False,
-        }, 404
 
 @books_bp.route("",methods=["POST","GET"])
 def books():
@@ -34,7 +18,7 @@ def books():
                 "description": book.description
             })
         return jsonify(books_response), 200
-    else:
+    elif request.method == "POST":
         request_body = request.get_json()
         new_book = Book(title = request_body["title"],
                         description = request_body["description"]
@@ -44,27 +28,30 @@ def books():
         
         return (f'book #{new_book.title} has been created', 201 )
 
-# hello_world_bp = Blueprint("hello_world", __name__)
-# @hello_world_bp.route('/hello-world', methods = ["GET"])
-# def get_hello_world():
-#     my_response = "Hello, World!"
-#     return my_response
+@books_bp.route("/<book_id>", methods=["GET","PUT"])
+def book(book_id):
+    book = Book.query.get(book_id)
 
-# @hello_world_bp.route('/hello-world/JSON', methods = ["GET"])
-# def hello_world_json():
-#     return{
-#         "name":"Abby C",
-#         "Message":"Hiya!",
-#         "Hobbies":["Baking", "Coding","Reading"] 
-#     },201
-
-# @hello_world_bp.route('/broken-endpoint-with-broken-server-code')
-# def broken_endpoint():
-#     response_body = {
-#         "name":"Abby C",
-#         "Message":"Hiya!",
-#         "Hobbies":["Baking", "Coding","Reading"] 
-#     }
-#     new_hobby = "Surfing"
-#     response_body["Hobbies"].append(new_hobby)
-#     return response_body
+    if request.method == "GET":
+        return ({
+            "id": book.id,
+            "title": book.title,
+            "description": book.description
+        }, 200)
+    #else
+    request_body = request.get_json()
+    try:
+        book.title = request_body["title"]
+        book.description = request_body["description"]
+        # save action
+        # db.session.add(book)
+        db.session.commit() 
+        return ({
+                "id": book.id,
+                "title": book.title,
+                "description": book.description
+            }, 200)
+    except KeyError:
+        return { 
+            "message":"Request requires both 'title' and 'description"
+        }, 400 
